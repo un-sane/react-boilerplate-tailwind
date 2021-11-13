49332c55041d8e021db1ae2cce851eeb5d1bba6c
Ananya Singh <ananyahimself@gmail.com>
2021-11-13 23:02:12 +0530

    [feat] favicon

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,10 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <React.Fragment>
-    <div className="flex flex-1 justify-center items-center bg-green-100">
-      <div className="transition-all duration-700 hover:shadow-lg hover:bg-green-700 p-12 rounded-xl group cursor-pointer">
-        <h1 className="transition-all duration-700 text-green-700 group-hover:text-white text-5xl font-bold">
-          Hello Kitty.
+    <div className="flex flex-1 justify-center items-center bg-purple-100">
+      <div className="transition-all duration-700 hover:shadow-lg hover:bg-purple-500 p-12 rounded-xl group cursor-pointer">
+        <h1 className="transition-all duration-700 text-purple-500 group-hover:text-white text-5xl font-bold">
+          Hi, I am Ananya.
         </h1>
       </div>
     </div>
